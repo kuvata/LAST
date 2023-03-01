4cb1a4dfcfa524e02a09b72c98aa126c1aaeb95b
Fudepia (Doumu) <fudepia@outlook.jp>
2023-03-01 23:06:54 +0800

    Add type alias `MEM`
    `MEM` is filtered `[SYM]` (with `...` and pointers removed)

--- Kuvaaja.hs
+++ Kuvaaja.hs
@@ -18,8 +18,10 @@ data SYM = Selector{eqn::EQN}
          | Object{eqn::EQN}
           deriving (Eq)
 instance Show SYM where
-    show x = a++"="++show b
-            where (a,b) = eqn x
+    show (Selector (a, b)) = a++"=S"++show b
+    show (Object (a, b)) = a++"=O"++show b
+    --show x = a++"="++show b
+    --        where (a,b) = eqn x
 instance Show Descriptor where
     show (Descriptor xs) = "{"++(intercalate ", " xs)++"}"
 mkDescriptor :: [String] -> Descriptor
@@ -27,6 +29,7 @@ mkDescriptor = Descriptor . nub
 --newtype SYM = SYM {eq::SYM} deriving (Eq)
 --instance Show SYM where
 --    show (SYM(a,b)) = a++"="++show b
+type MEM = [SYM]
 
 
 trimLRspaces = reverse.(dropWhile isSpace)
@@ -55,7 +58,7 @@ assignSYM :: (String, Descriptor) -> SYM
 assignSYM x@(_, xs)
             | "..." `elem` xs' = Object x
          -- | "..." `elem` (map (take 3) xs) = Object x
-            | otherwise = Object x
+            | otherwise = Selector x
              where xs' = content xs
 
 parseEQN :: Input -> [SYM]
@@ -73,15 +76,15 @@ splitBrackets s = if h=="{" then trimLRspaces (h++a):splitBrackets b
                  where (a,b) = break (=='{') s'
                        (h,s') = (splitAt 1).trimLRspaces$s
 
-query :: String -> State [SYM] (Maybe Descriptor)
+query :: String -> State MEM (Maybe Descriptor)
 query x = state $ f x
          where f x xs = (,xs)$snd.eqn <$> find ((==x).fst.eqn) xs
 
-replaceSym :: String -> [SYM] -> Descriptor
+replaceSym :: String -> MEM -> Descriptor
 replaceSym x s = case evalState (query x) s of
                    Just res -> res
                    Nothing -> error$"Cannot resolve symbol "++x
-replace :: String -> State [SYM] [String]
+replace :: String -> State MEM [String]
 replace x = state $ f x
            where f a@(k:xs) s = let x' = (dropWhile (not.(=='*')) a) in (,s)
                                $if (take 1 x')=="*"
@@ -90,47 +93,41 @@ replace x = state $ f x
 
 --evalRHS :: (String, [String]) -> [SYM] -> (String, String)
 --evalRHS :: (String, [String])
-evalRHS :: SYM -> State [SYM] EQN
+evalRHS :: SYM -> State MEM EQN
 evalRHS x = state $ f (eqn x)
 --f :: EQN -> [SYM] -> ((String, [String]), [SYM])
            where f (a,Descriptor b) s = (,s).(a,).mkDescriptor.fst
                   $foldM (flip runState) s (map replace b)
 
---mem :: [SYM]
---mem = [SYM("a", Descriptor["b"]), SYM("c", Descriptor["d"])]
--- parse :: String -> [SYM] ->
+eval :: MEM -> [SYM] -> [SYM] -- Not outputing `MEM` as it's `snd/state` needs to be further filtered through `loadSym'`
+eval s = (map (assignSYM.(`evalState` s).evalRHS)) -- Replace `*(VARNAME)` to it's content
 
-eval :: String -> State [SYM] [SYM]
-eval str = state$ f str
-          where f str s = dupe
-                         .(map (assignSYM
-                               .(`evalState` s).evalRHS)) -- Replace `*(VARNAME)` to it's content
-                         .parse$str
-                 --s = loadSym'.parse$str
-                -- genSYM (k,d) = assignSYM$(k, Descriptor (singleton d))
+exec :: String -> State MEM MEM
+exec str = state$ f str
+          where f str s = (\(a,b)->(a, map loadSym' b)).dupe
+                         .(eval s).parse$str
 
-loadSym :: [SYM] -> State [SYM] ()
+loadSym :: [SYM] -> State MEM ()
 loadSym = put.map loadSym'
 loadSym' :: SYM -> SYM
 loadSym' (Selector x) = Selector.(\(a,b) -> (a, Descriptor(filter ((/='*').(!!0)) (content b))))$x
-loadSym' (Object x)   = Object.(\(a,b) -> (a, Descriptor(filter ((/='*').(!!0)) (content b))))$x
---loadSym :: (Monad m) => [(String, String)] -> StateT [SYM] m ()
---loadSym = put.(map (SYM.(\(a,b) -> (a, '{':b++"}"))))
+loadSym' (Object x)   = Object.(\(a,b) -> (a, Descriptor(filter f (content b))))$x
+                       where f x = ((/='*').(!!0)$x) && x/="..."
+
+parse :: String -> [SYM]
 parse str = parseEQN.splitLines
            $str
 
 runNtimes 0 x0 = error "(runNtimes) `n`<1"
 runNtimes n x0 = loadSym (parse x0) >>= (\x1 -> runNtimes' n x0)
-runNtimes' 1 x0 = eval x0 >>= (\x3 -> return x3)
-runNtimes' n x0 = eval x0 >>= (\x2 -> runNtimes' (n-1) x0)
+runNtimes' 1 x0 = exec x0 >>= (\x3 -> return x3)
+runNtimes' n x0 = exec x0 >>= (\x2 -> runNtimes' (n-1) x0)
 -- TODO: Find better method
 repeatEval x0 = map (`evalState` []) (repeatEval' x0)
                where repeatEval' x0 = map runNtimes (iterate (+1) 1) <*> pure x0
 stableRes x0 = snd.head.(dropWhile (\(a,b) -> a/=b)) $ zip (repeatEval x0) (drop 1 (repeatEval x0))
 
---main=error""
-main = do
-    x0 <- getContents
+run x0 = do
     let res= stableRes x0
     let hist=(takeWhile (/=res) (repeatEval x0))++[res]
     print hist
@@ -138,3 +135,7 @@ main = do
     print $ length hist
     putStr "Result: "
     print res
+
+main = do
+    x0 <- getContents
+    run x0
