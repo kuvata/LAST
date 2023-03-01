241c632a97830be845a7d5a71a505f44403ddc5a
Fudepia (Doumu) <fudepia@outlook.jp>
2023-03-01 21:28:03 +0800

    Add subtypes (`Selector` and `Object`) to `SYM`

--- Kuvaaja.hs
+++ Kuvaaja.hs
@@ -1,4 +1,5 @@
 {-#LANGUAGE TupleSections #-}
+{-# LANGUAGE DerivingVia #-}
 
 import Data.Char
 import Data.List
@@ -6,6 +7,27 @@ import Control.Monad.Trans.State
 import Control.Monad
 import Data.Bifunctor (second)
 import Data.Tuple.Extra (dupe)
+
+
+
+newtype Descriptor = Descriptor {content::[String]}
+                        deriving (Eq)
+                        deriving (Semigroup, Monoid) via ([String])
+type EQN = (String, Descriptor)
+data SYM = Selector{eqn::EQN}
+         | Object{eqn::EQN}
+          deriving (Eq)
+instance Show SYM where
+    show x = a++"="++show b
+            where (a,b) = eqn x
+instance Show Descriptor where
+    show (Descriptor xs) = "{"++(intercalate ", " xs)++"}"
+mkDescriptor :: [String] -> Descriptor
+mkDescriptor = Descriptor . nub
+--newtype SYM = SYM {eq::SYM} deriving (Eq)
+--instance Show SYM where
+--    show (SYM(a,b)) = a++"="++show b
+
 
 trimLRspaces = reverse.(dropWhile isSpace)
               .reverse.(dropWhile isSpace)
@@ -28,8 +50,16 @@ parseBracket  ('{':xs) = if takeLast xs /= '}' then error "Descriptor doesn't en
                         where takeLast (_:ys) = if length ys == 1 then head ys else takeLast ys
 parseBracket  _ = error "Descriptor doesn't start with `{`"
 
-parseEQN :: Input -> [(String, [String])]
-parseEQN = map (second parseBracket)
+
+assignSYM :: (String, Descriptor) -> SYM
+assignSYM x@(_, xs)
+            | "..." `elem` xs' = Object x
+         -- | "..." `elem` (map (take 3) xs) = Object x
+            | otherwise = Object x
+             where xs' = content xs
+
+parseEQN :: Input -> [SYM]
+parseEQN = map (assignSYM.(second (Descriptor . parseBracket)))
           .splitEQUAL
 
 splitEQUAL :: Input -> [(String, String)]
@@ -43,68 +73,54 @@ splitBrackets s = if h=="{" then trimLRspaces (h++a):splitBrackets b
                  where (a,b) = break (=='{') s'
                        (h,s') = (splitAt 1).trimLRspaces$s
 
-newtype Descriptor = Descriptor {content::[String]} deriving (Eq)
-instance Show Descriptor where
-    show (Descriptor xs) = "{"++(intercalate ", " xs)++"}"
-mkDescriptor :: [String] -> Descriptor
-mkDescriptor = Descriptor . nub
-newtype SYM = SYM {eq::(String, Descriptor)} deriving (Eq)
-instance Show SYM where
-    show (SYM(a,b)) = a++"="++show b
 query :: String -> State [SYM] (Maybe Descriptor)
 query x = state $ f x
-         where f x xs = (,xs)$snd.eq <$> find ((==x).fst.eq) xs
+         where f x xs = (,xs)$snd.eqn <$> find ((==x).fst.eqn) xs
 
-replaceSym :: String -> [SYM] -> [String]
+replaceSym :: String -> [SYM] -> Descriptor
 replaceSym x s = case evalState (query x) s of
-                   Just res -> content res
+                   Just res -> res
                    Nothing -> error$"Cannot resolve symbol "++x
 replace :: String -> State [SYM] [String]
 replace x = state $ f x
            where f a@(k:xs) s = let x' = (dropWhile (not.(=='*')) a) in (,s)
                                $if (take 1 x')=="*"
-                                then replaceSym (drop 1 x') s-- ++"="++x'
+                                then content$replaceSym (drop 1 x') s-- ++"="++x'
                                 else singleton a
 
 --evalRHS :: (String, [String]) -> [SYM] -> (String, String)
 --evalRHS :: (String, [String])
-evalRHS x = state $ f x
-           where f (a,b) s = (,s).(a,).fst
-                            $foldM (flip runState) s (map replace b)
+evalRHS :: SYM -> State [SYM] EQN
+evalRHS x = state $ f (eqn x)
+--f :: EQN -> [SYM] -> ((String, [String]), [SYM])
+           where f (a,Descriptor b) s = (,s).(a,).mkDescriptor.fst
+                  $foldM (flip runState) s (map replace b)
 
-mem :: [SYM]
-mem = [SYM("a", Descriptor["b"]), SYM("c", Descriptor["d"])]
+--mem :: [SYM]
+--mem = [SYM("a", Descriptor["b"]), SYM("c", Descriptor["d"])]
 -- parse :: String -> [SYM] ->
+
+eval :: String -> State [SYM] [SYM]
 eval str = state$ f str
           where f str s = dupe
-                         .(map (SYM.(second mkDescriptor)
+                         .(map (assignSYM
                                .(`evalState` s).evalRHS)) -- Replace `*(VARNAME)` to it's content
                          .parse$str
                  --s = loadSym'.parse$str
-                genSYM (k,d) = SYM$(k, Descriptor (singleton d))
-loadSym' :: (Monad m) => [(String, [String])] -> StateT [SYM] m ()
-loadSym' = put.map (SYM.(second Descriptor).(\(a,b) -> (a, (filter ((/='*').(!!0)) b))))
+                -- genSYM (k,d) = assignSYM$(k, Descriptor (singleton d))
+
+loadSym :: [SYM] -> State [SYM] ()
+loadSym = put.map loadSym'
+loadSym' :: SYM -> SYM
+loadSym' (Selector x) = Selector.(\(a,b) -> (a, Descriptor(filter ((/='*').(!!0)) (content b))))$x
+loadSym' (Object x)   = Object.(\(a,b) -> (a, Descriptor(filter ((/='*').(!!0)) (content b))))$x
 --loadSym :: (Monad m) => [(String, String)] -> StateT [SYM] m ()
 --loadSym = put.(map (SYM.(\(a,b) -> (a, '{':b++"}"))))
 parse str = parseEQN.splitLines
            $str
---exec = (map (SYM.(\(a,b)->(a, '{':b++"}")))).fst.eval
 
---exec' s = (flip evalState s).eval
---exec = (flip evalState []).eval
-
---exec str = (evalState (eval str)).loadSym'$parse str
-doubleRun str = do
-                    m <- loadSym' $ parse str
-                    m <- eval str
-                    --m2 <- loadSym' m1
-                    m <- eval str
-                    m <- eval str
-                    return m
-
---runNtimes 1 x0 = loadSym' (parse x0) >>= (\x1 -> eval x0 >>= (\x2 -> return x2))
 runNtimes 0 x0 = error "(runNtimes) `n`<1"
-runNtimes n x0 = loadSym' (parse x0) >>= (\x1 -> runNtimes' n x0)
+runNtimes n x0 = loadSym (parse x0) >>= (\x1 -> runNtimes' n x0)
 runNtimes' 1 x0 = eval x0 >>= (\x3 -> return x3)
 runNtimes' n x0 = eval x0 >>= (\x2 -> runNtimes' (n-1) x0)
 -- TODO: Find better method
